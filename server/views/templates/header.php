6d33f9622749884d8135145c679f3a808143a0c6
chagonzles <charlene.gonzles@gmail.com>
2018-01-26 10:16:26 +0800

    Add bootstrap assets, rename application folder to server and update htaccess file

---
+++ server/views/templates/header.php
@@ -0,0 +1,17 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title><?= $title ?></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url() . 'assets/css/bootstrap.min.css' ?>">
+  </head>
+  <body>
+
